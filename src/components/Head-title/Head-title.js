4f0f7d47176a8204dc89f40fee9f09c740a2d192
szalo13 <kamil_sza@yahoo.pl>
2017-04-19 01:37:41 +0200

    Extending application skeleton for components

---
+++ src/components/Head-title/Head-title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Head-title.css';
+
+class HeadTitle extends Component {
+  render() {
+    return (
+      <div className="HeadTitle">
+        <h2>Welcome to React</h2>
+      </div>
+    )
+  }
+}
+
+export default HeadTitle;
